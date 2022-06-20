437b449bf7c269fcb77fdc011ca9bbc259ce6de8
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-06-20 12:11:45 +0900

    nomad coder 6.2 useEffect & deps

--- nomad-react/src/App.js
+++ nomad-react/src/App.js
@@ -1,11 +1,34 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import {useState, useEffect} from "react"
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setCounter((prev) => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value)
+
+  useEffect(()=>{
+    console.log("I run only once")
+  }, [])
+  useEffect(()=>{
+    if (keyword !== ""){
+      console.log("I run when keyword changes")
+    }
+  }, [keyword])
+  useEffect(()=>{
+    console.log("I run when counter changes")
+  }, [counter])
+
   return (
     <div>
-      <h1 className={styles.title}> helo </h1>
-      <Button text={"Continue"}/>
+      <input 
+        value={keyword} 
+        onChange ={onChange} 
+        type="text" 
+        placeholder="Serach here..."/>
+      <h1 className={styles.title}> {counter} </h1>
+      <button onClick={onClick}> click me </button>
     </div>
   );
 }
--- nomad-react/src/index.js
+++ nomad-react/src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+);
