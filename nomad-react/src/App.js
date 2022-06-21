a986aef379950d715382f0bbcb72bf567d41656f
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-06-21 11:24:45 +0900

    nomad coder 7.3 movie app part one

--- nomad-react/src/App.js
+++ nomad-react/src/App.js
@@ -3,22 +3,44 @@ import {useState, useEffect} from "react"
 
 function App() {
   const [loading, setLoading] = useState(true)
-  const [coins, setCoins] = useState([])
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async() => {
+    const response = await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+    const json = await response.json()
+    setMovies(json.data.movies)
+    setLoading(false)
+  }
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-    .then((response) => response.json())
-    .then((json) => {
-      setCoins(json)
-      setLoading(false)
-    })
+    getMovies()
   }, []);
+
+  // useEffect(() => {
+  //   fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")
+  //   .then((response)=>response.json())
+  //   .then((json)=> {
+  //     setMovies(json.data.movies)
+  //     setLoading(false)
+  //   })
+  // }, []);
+  console.log(movies)
   return (
     <div>
-      <h1> The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}</li>)}
-      </ul>
+      {loading ? <h1>Loading...</h1> : 
+      <div>
+        {movies.map((movie) => ( 
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <img src = {movie.medium_cover_image} />
+          <p>{movie.summary}</p>
+          {movie.hasOwnProperty("genres")? 
+          <ul>
+            {movie.genres.map((g) => (
+                <li key={g}>{g}</li>
+              ))}
+          </ul> : null}
+        </div>))}
+      </div>}
     </div>
   );
 }
