94baada576594dd756e414e790a9b00325e6a2f1
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-06-21 10:35:32 +0900

    nomad coder 7.2 coin tracker

--- nomad-react/src/App.js
+++ nomad-react/src/App.js
@@ -2,38 +2,23 @@ import {useState, useEffect} from "react"
 
 
 function App() {
-  const [toDo, setToDo] = useState("")
-  const [toDos, setToDos] = useState([])
-
-  const onChange = (event) => setToDo(event.target.value)
-
-  const onSubmit = (event) => {
-    event.preventDefault()
-    if (toDo === ""){
-      return;
-    }
-    setToDo("")
-    // state should be always new. make new array! 
-    setToDos((currentArray) => [toDo, ...currentArray])
-  }
-
-  console.log(toDos)
+  const [loading, setLoading] = useState(true)
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json)
+      setLoading(false)
+    })
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length}) </h1>
-      <form onSubmit={onSubmit}>
-        <input 
-        onChange={onChange} 
-        value={toDo} 
-        type="text" 
-        placeholder="Write your to do" />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item,index)=> (
-      <li key={index}>
-        {item}
-      </li>))}
+      <h1> The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin) => <li>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price}</li>)}
+      </ul>
     </div>
   );
 }
