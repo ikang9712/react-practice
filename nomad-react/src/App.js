b29372fe00f3e5e74e107bb9cb75786c110a875d
Inho Kang <inhokang@Inhos-MacBook-Pro.local>
2022-06-21 10:18:48 +0900

    nomad coder 7.0 todo list part one

--- nomad-react/src/App.js
+++ nomad-react/src/App.js
@@ -1,22 +1,31 @@
-import Button from "./Button"
-import styles from "./App.module.css"
 import {useState, useEffect} from "react"
 
-function Hello(){
-  useEffect(()=>{
-    console.log("created")
-    return () => console.log("destroyed")
-  },[])
-  return <h1> hello </h1>
-}
 
 function App() {
-  const [showing, setShowing] = useState(false)
-  const onClick = () => setShowing((prev) => !prev)
+  const [toDo, setToDo] = useState("")
+  const [toDos, setToDos] = useState([])
+  const onChange = (event) => setToDo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (toDo === ""){
+      return;
+    }
+    setToDo("")
+    // state should be always new. make new array! 
+    setToDos((currentArray) => [toDo, ...currentArray])
+  }
+  console.log(toDos)
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide": "Show"}</button>
+      <h1>My To Dos ({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input 
+        onChange={onChange} 
+        value={toDo} 
+        type="text" 
+        placeholder="Write your to do" />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
